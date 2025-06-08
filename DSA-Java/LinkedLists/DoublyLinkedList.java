e81e91d4236aa9ef5de7ba5af70a9636fe34d36d
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-06-08 21:52:14 +0530

    Doubly Linked List (Insert, Delete, Display)

---
+++ DSA-Java/LinkedLists/DoublyLinkedList.java
@@ -0,0 +1,45 @@
+class DNode{
+    int data;
+    DNode prev, Next;
+    DNode(int data){
+        this.data = data;
+        this.prev = this.Next = null;
+    }
+}
+public class DoublyLinkedList {
+    DNode Head;
+    public void insert(int data){
+        DNode newNode = new DNode(data);
+        if(Head == null){
+            Head = newNode;
+            return;
+        }
+        DNode current = Head;
+        while(current.Next != null){
+            current = current.Next;
+        }
+        current.Next = newNode;
+        newNode.prev = current;
+            
+    }
+    public void delete(int value){
+        if (Head == null) return;
+        DNode current = Head;
+
+        if(current.data == value){
+            Head = current.Next;
+            if(Head != null) Head.prev = null;
+            return;
+        }
+        while (current != null && current.data != value){
+            current = current.Next;
+        }
+        if(current == null){
+            if(current.prev != null) current.prev.Next = current.Next;
+            if(current.Next!= null) current.Next.prev = current.prev;
+        }
+
+
+    }
+    
+}
